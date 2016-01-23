c47226e757dc0d04380b90071cd706547b7f0c12
xufeifandj <459821731@qq.com>
2016-01-23 18:19:48 +0800

    实现更换壁纸，拉动窗帘的动画效果

---
+++ app/src/main/java/xu/ferris/flymeshview/ChangeWallpaperView.java
@@ -0,0 +1,24 @@
+package xu.ferris.flymeshview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * 实现更换壁纸，拉动窗帘的动画效果
+ * 459821731@qq.com
+ * Created by ferris on 2016/1/23.
+ */
+public class ChangeWallpaperView extends FrameLayout {
+    public ChangeWallpaperView(Context context) {
+        super(context);
+    }
+
+    public ChangeWallpaperView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ChangeWallpaperView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
